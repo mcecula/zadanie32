45efef2a3efe73b30f20854c162d4f5b35d28534
mcecula <mc23051988@gmail.com>
2023-03-09 12:19:54 +0100

    poprawka

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 let num1 = 2;
 let num2 = 5;
 
-function num1num2(num1, num2){
-if (num1 > 10 || num2 > 10) {
-    return num1 * num2;
-}
-return 20;
-}
+function num1num2(num1, num2) {
+    if (num1 > num2) {
 
-console.log(num1num2(2,10))
+        return num1;
+
+    } else {
+
+        return num2;
+    }}
+
+    console.log(num1num2(20, 10))
